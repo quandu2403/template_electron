fb666a96e56993feeaf1a3f5097ce950ce545525
Quan Du <quandu2403@gmail.com>
2024-03-22 00:30:38 +0700

    first commit

---
+++ src/renderer/src/main.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '../../style/style.css'
+
+import App from './App'
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
